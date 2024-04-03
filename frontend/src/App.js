8e320eeae0bb36bf1d777516dd20526107a98be3
Ayush Raj <ayushrajar000@gmail.com>
2024-04-03 23:56:49 +0530

    Added home page and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/layout/Header/Header';
 import WebFont from 'webfontloader';
 import { useEffect } from 'react';
 import Footer from './components/layout/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,11 @@ function App() {
   return (
     <Router>
       <Header />
+
+      <Routes>
+        <Route exact path='/' Component={Home} />
+      </Routes>
+      
       <Footer />
     </Router>
   );
