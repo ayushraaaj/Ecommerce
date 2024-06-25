2d667ccbdb6ba658253c8a50afdf20a02ed47a09
Ayush Raj <ayushrajar000@gmail.com>
2024-06-25 22:45:49 +0530

    Modified home page and added products in home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import './Home.css'
+import Product from "./Product";
+
+const product = {
+    name: 'Blue Tshirt',
+    price: '₹4000',
+    images: [{ url: 'https://m7clothing.in/admin/images/m73171076384540467.jpg' }],
+    _id: '1234567890'
+}
 
 const Home = () => {
     return (
@@ -14,6 +22,19 @@ const Home = () => {
                         Scroll <CgMouse />
                     </button>
                 </a>
+            </div>
+
+            <h2 className="homeHeading">Featured Products</h2>
+
+            <div className="container" id="container">
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
             </div>
         </Fragment>
     );
